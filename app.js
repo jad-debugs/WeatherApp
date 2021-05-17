8c961f4b981b2c044f4d1600d7024b2eedaa5bf3
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-17 14:39:30 -0700

    created function to take any location name and return latitute/longitute

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 const url = 'http://api.weatherstack.com/current?access_key=c4de2159095c8dedb0f14ee66143aa7a&query=37.8267,-122.4233&units=f'
 
-request({url: url, json: true}, (error, response) => {
-    console.log("It is currently " + response.body.current.temperature
-    + " degrees outside, but it feels like " + response.body.current.feelslike)
-})
+// request({url: url, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service')
+//     }
+//     else if (response.body.error) {
+//         console.log('Unable to find')
+//     }
+//     else {
+//         console.log("It is currently " + response.body.current.temperature
+//         + " degrees outside, but it feels like " + response.body.current.feelslike)
+//     }
+// })
 
-const geocodeURL = 'http://api.positionstack.com/v1/forward?access_key=83f21002a9446fd4be93fa7f46815f10&query=Los%20Angeles&limit=1'
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?limit=1&access_token=pk.eyJ1IjoiamFkLWRlYnVncyIsImEiOiJja290MXJvMnAwMzJ6MnVvMzc2aDhrOTcwIn0.gLqfFoAdKbokpU3-zuH7nw'
 
-request({url: geocodeURL, json: true}, (error, response) => {
-    console.log('latitude: ' + response.body.data[0].latitude + 
-    '\nlongitude: ' + response.body.data[0].longitude);
+// request({url: geocodeURL, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('Could not connect to map box')
+//     }
+//     else if (response.body.features.length === 0) {
+//         console.log('Unable to find location. Try another search')
+//     }
+//     else {
+//         console.log('latitude: ' + response.body.features[0].center[1] + 
+//         '\nlongitude: ' + response.body.features[0].center[0]);
+//     }
+// })
+
+geocode('California', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
---
+++ utils/geocode.js
@@ -0,0 +1,23 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?limit=1&access_token=pk.eyJ1IjoiamFkLWRlYnVncyIsImEiOiJja290MXJvMnAwMzJ6MnVvMzc2aDhrOTcwIn0.gLqfFoAdKbokpU3-zuH7nw'
+
+    request({url: url, json: true}, (error, response) => {
+        if (error) {
+            callback('Unable to connect to connect to map box!', undefined)
+        }
+        else if (response.body.features.length === 0) {
+            callback('Unable to find location. Try another search!', undefined)
+        }
+        else {
+            callback(undefined, {
+                latitute: response.body.features[0].center[1],
+                longitute: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
