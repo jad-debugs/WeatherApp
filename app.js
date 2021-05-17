5de03fe5194376ad524f135beacba786f0a47bfd
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-17 00:11:37 -0700

    using position stack geocoder api

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-console.log("starting")
+const request = require('request')
 
-setTimeout(() => {
-    console.log("waiting 2 seconds")
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=c4de2159095c8dedb0f14ee66143aa7a&query=37.8267,-122.4233&units=f'
 
-console.log("ending")
+request({url: url, json: true}, (error, response) => {
+    console.log("It is currently " + response.body.current.temperature
+    + " degrees outside, but it feels like " + response.body.current.feelslike)
+})
+
+const geocodeURL = 'http://api.positionstack.com/v1/forward?access_key=83f21002a9446fd4be93fa7f46815f10&query=Los%20Angeles&limit=1'
+
+request({url: geocodeURL, json: true}, (error, response) => {
+    console.log('latitude: ' + response.body.data[0].latitude + 
+    '\nlongitude: ' + response.body.data[0].longitude);
+})
