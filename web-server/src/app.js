23e4542adc160520d419c6472845b6087edd0e58
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-17 23:55:19 -0700

    basic express server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,5 +3,21 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('Home Page')
+})
+
+app.get('/help', (req, res) => {
+    res.send('Help Page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About Page')
+})
+app.get('/weather', (req, res) => {
+    res.send('Weather Page')
+})
+
+// starts up website
+app.listen(3000, () => {
+    console.log("server is up on port 3000")
 })
