4571dd2764f2e9dd9acb87bcd9b839673cc3cd6c
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-19 12:22:51 -0700

    using fetch on client side js

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side js is loaded')
+
+fetch('http://localhost:3000/weather?address=12wh').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        }
+        else {
+            console.log(data.location + ' ' + data.forecast)
+        }
+    })
+})
