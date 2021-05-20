769a17e725f241511900d4b6fe4712b210b83e80
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-19 18:34:06 -0700

    created search form that fetches from our apis to print all info

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,24 @@
 console.log('Client side js is loaded')
 
-fetch('http://localhost:3000/weather?address=12wh').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        }
-        else {
-            console.log(data.location + ' ' + data.forecast)
-        }
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = searchElement.value
+    const url = 'http://localhost:3000/weather?address=' + location
+
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            }
+            else {
+                console.log(data.location + ' ' + data.forecast)
+            }
+        })
     })
+
 })
